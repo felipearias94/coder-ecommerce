1278f7118baa113928930a5fb4a3ea018c315d4d
Felipe <felari94@gmail.com>
2023-10-16 19:20:22 -0300

    persist session data with asyncStorage

--- src/navigation/MainNav.jsx
+++ src/navigation/MainNav.jsx
@@ -1,15 +1,30 @@
 import { StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigation from './TabNavigation';
 import AuthNav from './AuthNav';
 import { useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const MainNav = () => {
+  const [checkedUser, setCheckedUser] = useState(null);
   const user = useSelector((state) => state.authSlice.user);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const userEmail = await AsyncStorage.getItem('userEmail');
+        userEmail ? setCheckedUser(userEmail) : setCheckedUser(user);
+        console.log(userEmail);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkUser();
+  }, []);
   return (
     <NavigationContainer>
-      {user ? <TabNavigation /> : <AuthNav />}
+      {checkedUser ? <TabNavigation /> : <AuthNav />}
     </NavigationContainer>
   );
 };
--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createSlice } from '@reduxjs/toolkit';
 
 const authSlice = createSlice({
@@ -9,13 +10,16 @@ const authSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
+      AsyncStorage.setItem('userEmail', action.payload);
     },
     setIdToken: (state, action) => {
       state.idToken = action.payload;
+      AsyncStorage.setItem('userToken', action.payload);
     },
     clearUser: (state) => {
       state.user = null;
       state.idToken = null;
+      AsyncStorage.multiRemove(['userEmail', 'userToken']);
     },
   },
 });
